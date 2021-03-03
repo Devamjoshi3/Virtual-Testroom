d8af69dadf4acdaa7bfbe180f1a1ef0232a3037c
Devamjoshi3 <devamjoshi3@gmail.com>
2021-03-03 17:47:09 +0530

    Done

---
+++ app/Http/Controllers/Admin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Oex_category;
+use Validator;
+class Admin extends Controller
+{
+    public function index()
+	{
+		return view('admin.dashboard');
+	}
+	public function exam_category()
+	{
+		$data['category']=Oex_category::orderBy('id','desc')->get()->toArray();
+		return view('admin.exam_category',$data);
+	}
+	public function add_new_category(Request $request)
+	{
+		$validator=Validator::make($request->all(),['name'=>'required']);
+		if($validator->passes())
+		{
+			$cat = new Oex_category();
+			$cat->name=$request->name;
+			$cat->status=1;
+			$cat->save();
+			$arr=array('status'=>'true','message'=>'success','reload'=>url('/admin/exam_category'));
+		}
+		else
+		{
+			$arr=array('status'=>'false','message'=>$validator->errors()->all());
+		}
+		echo json_encode($arr);
+	}
+	public function delete_category($id)
+	{
+		$cat = Oex_category::where('id',$id)->get()->first();
+		$cat->delete();
+		return redirect(url('admin/exam_category'));
+	}
+	public function edit_category($id)
+	{
+		$category = Oex_category::where('id',$id)->get()->first();
+		return view('admin.edit_category',['category'=>$category]);
+	}
+	public function edit_new_category(Request $request)
+	{
+		$cat = Oex_category::where('id',$request->id)->get()->first();
+		$cat->name=$request->name;
+		$cat->update();
+		echo json_encode(array('status'=>'true','message'=>'Category Successfully Updated','reload'=>url('/admin/exam_category')));
+	}
+
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin','Admin@index');
+Route::get('/admin/exam_category','Admin@exam_category');
+Route::post('/admin/add_new_category','Admin@add_new_category');
+Route::get('/admin/delete_category/{id}','Admin@delete_category');
+Route::get('/admin/edit_category/{id}','Admin@edit_category');
+Route::post('/admin/edit_new_category','Admin@edit_new_category');
